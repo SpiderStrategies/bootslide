6d45d9a44a8654d9d0c926cbab8a51c3eb54398e
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2014-01-14 14:41:45 -0500

    Turns this into a npm module. Bumps version

--- example/bundle.js
+++ example/bundle.js
@@ -1,3 +1,4 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"02Bbs6":[function(require,module,exports){
 var Bootslide = function (menu, opts) {
   var opts = opts || {}
   this.width = opts.width || 250
@@ -107,3 +108,7 @@ Bootslide.prototype.buildSections = function (menu, sections, back) {
 }
 
 module.exports = Bootslide
+
+},{}],"bootslide":[function(require,module,exports){
+module.exports=require('02Bbs6');
+},{}]},{},[])
